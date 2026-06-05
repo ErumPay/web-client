de6254cc3fb5eeea2f06ef4bf03cc711e8b65bfc
choboreum <choboreum@naver.com>
2026-06-05 10:30:01 +0900

    [KAN-1372] feat: web-merchant 초기 프로젝트 복제

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ document.title = import.meta.env.VITE_APP_TITLE || "ErumPay 어드민 - 슈퍼�
 
 Object.assign(window, { React, ReactDOM });
 
-import("./web-pg/icons.jsx")
+const service = import.meta.env.VITE_APP_SERVICE || "web-pg";
+
+const importPg = () => import("./web-pg/icons.jsx")
   .then(() => import("./web-pg/components.jsx"))
   .then(() => import("./web-pg/layout.jsx"))
   .then(() => import("./web-pg/data.jsx"))
@@ -16,3 +18,16 @@ import("./web-pg/icons.jsx")
   .then(() => import("./web-pg/screens/settlements.jsx"))
   .then(() => import("./web-pg/screens/other.jsx"))
   .then(() => import("./web-pg/app.jsx"));
+
+const importMerchant = () => import("./web-merchant/icons.jsx")
+  .then(() => import("./web-merchant/components.jsx"))
+  .then(() => import("./web-merchant/layout.jsx"))
+  .then(() => import("./web-merchant/data.jsx"))
+  .then(() => import("./web-merchant/screens/dashboard.jsx"))
+  .then(() => import("./web-merchant/screens/merchants.jsx"))
+  .then(() => import("./web-merchant/screens/transactions.jsx"))
+  .then(() => import("./web-merchant/screens/settlements.jsx"))
+  .then(() => import("./web-merchant/screens/other.jsx"))
+  .then(() => import("./web-merchant/app.jsx"));
+
+(service === "web-merchant" ? importMerchant : importPg)();
